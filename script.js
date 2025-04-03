ff628e654d8dedd9e969fa9834206af9efe7a6a9
shravan kumar <117439152+borrolla@users.noreply.github.com>
2025-04-03 23:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+
+const video = document.querySelector(".flex");
+const toggle = document.querySelector(".toggle");
+const progress = document.querySelector(".progress");
+const volume = document.querySelector('.volume');
+const playbackSpeed = document.querySelector(".playbackSpeed");
+const rewind = document.querySelector(".rewind");
+const forward = document.querySelector(".forward");
+const speedBar = document.querySelector(".speed-bar");
+
+
+function togglePlay(){
+	if(video.paused){
+		video.play();
+		toggle.textContent = "❚ ❚";
+	}else{
+		video.pause();
+		toggle.textContent = "►";
+	}
+}
+
+toggle.addEventListener("click", togglePlay);
+video.addEventListener("play", () => toggle.textContent = "❚ ❚");
+video.addEventListener("pause", () => toggle.textContent = "►");
+
+function updateProgress(){
+	const percentage = (video.currentTime / video.duration) * 100;
+	progress.value = percentage;
+}
+
+video.addEventListener("timeupdate", updateProgress);
+progress.addEventListener("input", (e) => {
+    video.currentTime = (e.target.value / 100) * video.duration;
+});
+
+volume.addEventListener("input", (e) => {
+	video.volume = e.target.value;
+});
+
+playbackSpeed.addEventListener("input", (e) => {
+	video.playbackRate = e.target.value;
+	speedBar.textContent = e.target.value +  "x";
+});
+
+rewind.addEventListener("click", () => {
+	video.currentTime = Math.max(0, video.currentTime - 10);
+});
+
+forward.addEventListener("click", () => {
+    video.currentTime = Math.min(video.duration, video.currentTime + 25);
+});
+
 const inputs = document.querySelectorAll('.controls input');
 
     function handleUpdate() {
@@ -7,3 +59,5 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+
